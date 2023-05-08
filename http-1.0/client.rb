a53898e40d05ff79c2e2227f28ad9a0f5a8d0808
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-05-08 16:57:15 +0900

    Add example of headers.

--- http-1.0/client.rb
+++ http-1.0/client.rb
@@ -10,8 +10,20 @@ Sync do
 	ARGV.each do |path|
 		endpoint.connect do |connection|
 			stream = Async::IO::Stream.new(connection)
-			stream.write("GET #{path} HTTP/1.0\r\n\r\n")
+			stream.write("GET #{path} HTTP/1.0\r\n")
+			stream.write("Accept: text/html\r\n")
+			stream.write("\r\n")
 			stream.flush
+			
+			response_line = stream.read_until("\r\n")
+			version, status, reason = response_line.split(' ', 3)
+			Console.logger.info(self, "Received #{version} #{status} #{reason}")
+			
+			while line = stream.read_until("\r\n")
+				break if line.empty?
+				name, value = line.split(/:\s*/, 2)
+				Console.logger.info(self, "Header: #{name} = #{value}")
+			end
 			
 			puts stream.read
 		end
